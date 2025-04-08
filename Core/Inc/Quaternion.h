49775955739f461de8dd29e815f3ce61c17a1a51
Piotr Woliński <101202778+Koun7z@users.noreply.github.com>
2025-04-08 16:12:12 +0200

    Fixed some errors + added docs to quaternion math

--- Core/Inc/Quaternion.h
+++ Core/Inc/Quaternion.h
@@ -5,6 +5,11 @@
 #ifndef QUATERNION_H
 #define QUATERNION_H
 
+/**
+ * @brief Structure representing a quaternion q = (r, i, j, k):
+ *		  - r       field denotes the real part
+ *		  - i, j, k fields denote the corresponding imaginary parts
+ */
 typedef struct
 {
 	float r;
@@ -13,22 +18,104 @@ typedef struct
 	float k;
 } DSP_Quaternion_f32;
 
+/**
+ * @brief		Performs quaternion addition operation and puts the result into dst output parameter.
+ *
+ *				dst = q1 + q2
+ *
+ * @param[out] *dst  Operation output
+ * @param[in]  *q1   First input quaternion
+ * @param[in]  *q2   Second input quaternion
+ */
 void DSP_QT_Add_f32(DSP_Quaternion_f32* dst, const DSP_Quaternion_f32* q1, const DSP_Quaternion_f32* q2);
 
-void DSP_QT_Subtract_f32(DSP_Quaternion_f32* dst,const DSP_Quaternion_f32* q1,const DSP_Quaternion_f32* q2);
+/**
+ * @brief		Performs quaternion subtraction operation and puts the result into dst output parameter.
+ *
+ *				dst = q1 - q2
+ *
+ * @param[out] *dst  Operation output
+ * @param[in]  *q1   First input quaternion
+ * @param[in]  *q2   Second input quaternion
+ */
+void DSP_QT_Subtract_f32(DSP_Quaternion_f32* dst, const DSP_Quaternion_f32* q1, const DSP_Quaternion_f32* q2);
 
-void DSP_QT_Multiply_f32(DSP_Quaternion_f32* dst,const DSP_Quaternion_f32* q1,const DSP_Quaternion_f32* q2);
+/**
+ * @brief		Performs quaternion multiplication (Hamilton product)
+ *				operation and puts the result into dst output parameter.
+ *
+ *				dst = q1 * q2
+ *
+ * @param[out] *dst  Operation output
+ * @param[in]  *q1   First input quaternion
+ * @param[in]  *q2   Second input quaternion
+ */
+void DSP_QT_Multiply_f32(DSP_Quaternion_f32* dst, const DSP_Quaternion_f32* q1, const DSP_Quaternion_f32* q2);
 
-void DSP_QT_Scale_f32(DSP_Quaternion_f32* dst,const DSP_Quaternion_f32* q,float scale);
+/**
+ * @brief		Performs multiplication of given quaternion by given scalar value
+ *				and puts the result in dst output parameter.
+ *
+ *				dst = s * q
+ *
+ * @param[out] *dst  Operation output
+ * @param[in]  *q    Input quaternion
+ * @param[in]   s    Scalar value
+ */
+void DSP_QT_Scale_f32(DSP_Quaternion_f32* dst, const DSP_Quaternion_f32* q, float s);
 
-void DSP_QT_Conjugate_f32(DSP_Quaternion_f32* dst,const DSP_Quaternion_f32* q);
+/**
+ * @brief		Performs quaternion conjugation operation and puts the result into dst output parameter.
+ *
+ *				dst = conj(q)
+ *
+ * @param[out] *dst  Operation output
+ * @param[in]  *q    Input quaternion
+ */
+void DSP_QT_Conjugate_f32(DSP_Quaternion_f32* dst, const DSP_Quaternion_f32* q);
 
-void DSP_QT_Normalized_f32(DSP_Quaternion_f32* dst,const DSP_Quaternion_f32* q);
+/**
+ * @brief		Performs quaternion normalization and puts the resulting unit quaternion into dst output parameter.
+ *
+ *				dst = q / ||q||
+ *
+ * @param[out] *dst  Operation output
+ * @param[in]  *q    Input quaternion
+ */
+void DSP_QT_Normalize_f32(DSP_Quaternion_f32* dst, const DSP_Quaternion_f32* q);
 
+/**
+ * @brief	   Calculates the quaternion norm as the square root of the product of a quaternion with its conjugate.
+ *
+ *			   dst = ||q|| = sqrt(q * conj(q))
+ *
+ * @param[in] *q      Input quaternion
+ * @retval     float  Quaternion norm
+ */
 float DSP_QT_Norm_f32(const DSP_Quaternion_f32* q);
 
-
+/**
+ * @brief	    Converts a unit quaternion, representing a rotation in 3d space
+ *				into Euler angles representation in Tait-Bryan convention,
+ *				using the body 3-2-1 sequence <=> lab 1-2-3 sequence.
+ *
+ *				eulerAngles = [Roll (x), Pitch (y), Yaw (z)]
+ *
+ * @param[out] *eulerAngles  Roll, Pitch and Yaw as Euler angles in radians
+ * @param[in]  *q			 Input unit quaternion representing the rotation
+ */
 void DSP_QT_EulerAngles_f32(float* eulerAngles, const DSP_Quaternion_f32* q);
 
-void DSP_QT_EulerToQuaternion(DSP_Quaternion_f32* q, const float* eulerAngles);
-#endif //QUATERNION_H
+/**
+ * @brief		Converts a rotation described in Euler angels, in Tait-Bryan convention
+ *				into a unit quatetnion representation using
+ *				Body 3-2-1 sequence <=> Lab 1-2-3 sequence
+ *
+ *				eulerAngles = [Roll (x), Pitch (y), Yaw (z)]
+ *
+ * @param[out] *q            Rotation representation in unit quaternion
+ * @param[in]  *eulerAngles  Roll, Pitch and Yaw as float array
+ */
+void DSP_QT_EulerToQuaternion_f32(DSP_Quaternion_f32* q, const float* eulerAngles);
+
+#endif  // QUATERNION_H
--- Core/Src/MathematicalStructures/Quaternion_f32.c
+++ Core/Src/MathematicalStructures/Quaternion_f32.c
@@ -43,12 +43,12 @@ void DSP_QT_Multiply_f32(DSP_Quaternion_f32* dst, const DSP_Quaternion_f32* q1, 
 	dst->k = a1 * d2 + b1 * c2 - c1 * b2 + d1 * a2;
 }
 
-void DSP_QT_Scale_f32(DSP_Quaternion_f32* dst, const DSP_Quaternion_f32* q, float scale)
+void DSP_QT_Scale_f32(DSP_Quaternion_f32* dst, const DSP_Quaternion_f32* q, float s)
 {
-	dst->r = scale * q->r;
-	dst->i = scale * q->i;
-	dst->j = scale * q->j;
-	dst->k = scale * q->k;
+	dst->r = s * q->r;
+	dst->i = s * q->i;
+	dst->j = s * q->j;
+	dst->k = s * q->k;
 }
 
 void DSP_QT_Conjugate_f32(DSP_Quaternion_f32* dst, const DSP_Quaternion_f32* q)
@@ -59,7 +59,7 @@ void DSP_QT_Conjugate_f32(DSP_Quaternion_f32* dst, const DSP_Quaternion_f32* q)
 	dst->k = -q->k;
 }
 
-void DSP_QT_Normalized_f32(DSP_Quaternion_f32* dst, const DSP_Quaternion_f32* q)
+void DSP_QT_Normalize_f32(DSP_Quaternion_f32* dst, const DSP_Quaternion_f32* q)
 {
 	const float norm = sqrtf(q->r * q->r + q->i * q->i + q->j * q->j + q->k * q->k);
 
@@ -78,26 +78,26 @@ void DSP_QT_EulerAngles_f32(float* eulerAngles, const DSP_Quaternion_f32* q)
 {
 	const float sing = q->i * q->j + q->k * q->r;
 
-	if (sing > QT_SINGULARITY_THRESHOLD)
+	if(sing > QT_SINGULARITY_THRESHOLD)
 	{
-		eulerAngles[2] = 2 * atan2f(q->i, q->r);
-		eulerAngles[1] = PI_F32 / 2;
-		eulerAngles[0] = 0;
+		eulerAngles[2] = 2.0f * atan2f(q->i, q->r);
+		eulerAngles[1] = PI_F32 / 2.0f;
+		eulerAngles[0] = 0.0f;
 	}
-	else if (sing < -QT_SINGULARITY_THRESHOLD)
+	else if(sing < -QT_SINGULARITY_THRESHOLD)
 	{
-		eulerAngles[2] = -2 * atan2f(q->i, q->r);
-		eulerAngles[1] = PI_F32 / 2;
-		eulerAngles[0] = 0;
+		eulerAngles[2] = -2.0f * atan2f(q->i, q->r);
+		eulerAngles[1] = PI_F32 / 2.0f;
+		eulerAngles[0] = 0.0f;
 	}
 
-	eulerAngles[0] = atan2f(2 * (q->r * q->i + q->j * q->k), 1 - 2 * (q->i * q->i + q->j * q->j));
-	eulerAngles[1] =
-	  -PI_F32 / 2 + 2 * atan2(sqrtf(1 + 2 * (q->r * q->j - q->i * q->k)), sqrtf(1 - 2 * (q->r * q->j + q->i * q->k)));
-	eulerAngles[2] = atan2(2 * (q->r * q->k + q->i * q->j), 1 - 2 * (q->j * q->j + q->k * q->k));
+	eulerAngles[0]    = atan2f(2.0f * (q->r * q->i + q->j * q->k), 1 - 2 * (q->i * q->i + q->j * q->j));
+	const float sq_qt = 2.0f * (q->r * q->j - q->i * q->k);
+	eulerAngles[1]    = -PI_F32 / 2.0f + 2.0f * atan2f(sqrtf(1.0f + sq_qt), sqrtf(1.0f - sq_qt));
+	eulerAngles[2]    = atan2f(2.0f * (q->r * q->k + q->i * q->j), 1.0f - 2.0f * (q->j * q->j + q->k * q->k));
 }
 
-void DSP_QT_EulerToQuaternion(DSP_Quaternion_f32* q, const float* eulerAngles)
+void DSP_QT_EulerToQuaternion_f32(DSP_Quaternion_f32* q, const float* eulerAngles)
 {
 	const float cosX = cosf(eulerAngles[0] * 0.5f);
 	const float sinX = sinf(eulerAngles[0] * 0.5f);
