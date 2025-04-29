75a362f783a6cf2f44332cbfbf486dbf6cfb8429
Piotr Woliński <101202778+Koun7z@users.noreply.github.com>
2025-04-29 18:22:18 +0200

    Fixed IIR PID

--- Core/Src/ProcessControl/DSP_SimplePID_f32.c
+++ Core/Src/ProcessControl/DSP_SimplePID_f32.c
@@ -19,7 +19,7 @@ void DSP_SimplePID_Init_f32(DSP_SimplePID_Instance_f32* regulator,
 	regulator->A[2] = Kd / Ts;
 
 	// Zero out the past samples
-
+	regulator->output      = 0.0f;
 	regulator->_prevErr[0] = 0.0f;
 	regulator->_prevErr[1] = 0.0f;
 }
@@ -27,8 +27,8 @@ void DSP_SimplePID_Init_f32(DSP_SimplePID_Instance_f32* regulator,
 float DSP_SimplePID_Update_f32(DSP_SimplePID_Instance_f32* regulator, const float err)
 {
 	// Update PID
-	regulator->output =
-	  err * regulator->A[0] + regulator->_prevErr[0] * regulator->A[1] + regulator->_prevErr[1] * regulator->A[2];
+	regulator->output = regulator->output + err * regulator->A[0] + regulator->_prevErr[0] * regulator->A[1]
+	                  + regulator->_prevErr[1] * regulator->A[2];
 
 	// Shift past samples
 	regulator->_prevErr[1] = regulator->_prevErr[0];
