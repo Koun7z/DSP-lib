da718364c7bc180690e31c3f40a938db1467b9cf
Piotr Woliński <101202778+Koun7z@users.noreply.github.com>
2026-02-15 17:48:37 +0100

    refactor: Chenged NC filter to represent attitude as local -> global transformation

--- Core/Src/AHRS/DSP_AHRS_NC_f32.c
+++ Core/Src/AHRS/DSP_AHRS_NC_f32.c
@@ -29,7 +29,6 @@ void DSP_AHRS_NC_FilterUpdate_f32(const DSP_AHRS_NC_Instance_f32* filter,
     const float q = data->GyroData[1];
     const float r = data->GyroData[2];
 
-
     float a_i = data->AccData[0];
     float a_j = data->AccData[1];
     float a_k = data->AccData[2];
@@ -39,8 +38,8 @@ void DSP_AHRS_NC_FilterUpdate_f32(const DSP_AHRS_NC_Instance_f32* filter,
 
     DSP_Quaternion_f32 delta_q_gyro;
 
-    DSP_QT_Multiply_f32(&delta_q_gyro, &omega_l, &data->AttitudeEstimate);
-    DSP_QT_Scale_f32(&delta_q_gyro, &delta_q_gyro, -0.5f * dt);
+    DSP_QT_Multiply_f32(&delta_q_gyro, &data->AttitudeEstimate, &omega_l);
+    DSP_QT_Scale_f32(&delta_q_gyro, &delta_q_gyro, 0.5f * dt);
 
     DSP_Quaternion_f32 q_gyro;
 
@@ -56,32 +55,29 @@ void DSP_AHRS_NC_FilterUpdate_f32(const DSP_AHRS_NC_Instance_f32* filter,
     a_k = a_k / acc_norm;
 
     // Calculating predicted gravity vector
-    float G_pred[3] = {a_i, a_j, a_k};
-    DSP_Quaternion_f32 q_gyro_LG;
-    DSP_QT_Conjugate_f32(&q_gyro_LG, &q_gyro);
+    float g_pred[3] = {a_i, a_j, a_k};
 
-    DSP_QT_RotateVector_f32(G_pred, G_pred, &q_gyro_LG);
-
+    DSP_QT_RotateVector_f32(g_pred, g_pred, &q_gyro);
 
     // Calculating accelerometer correction
     DSP_Quaternion_f32 delta_q_acc;
-    if(G_pred[2] >= 0.0f)
+    if(g_pred[2] >= 0.0f)
     {
-        const float sq_gk = sqrtf(2.0f * (G_pred[2] + 1.0f));
+        const float sq_gk = sqrtf(2.0f * (g_pred[2] + 1.0f));
 
-        delta_q_acc.r = sqrtf((G_pred[2] + 1.0f) / 2.0f);
-        delta_q_acc.i = -G_pred[1] / sq_gk;
-        delta_q_acc.j = G_pred[0] / sq_gk;
+        delta_q_acc.r = sqrtf((g_pred[2] + 1.0f) / 2.0f);
+        delta_q_acc.i = g_pred[1] / sq_gk;
+        delta_q_acc.j = -g_pred[0] / sq_gk;
         delta_q_acc.k = 0.0f;
     }
     else
     {
-        const float sq_gk = sqrtf(2.0f * (1.0f - G_pred[2]));
+        const float sq_gk = sqrtf(2.0f * (1.0f - g_pred[2]));
 
-        delta_q_acc.r = -G_pred[1] / sq_gk;
-        delta_q_acc.i = sqrtf((1.0f - G_pred[2]) / 2.0f);
+        delta_q_acc.r = -g_pred[1] / sq_gk;
+        delta_q_acc.i = -sqrtf((1.0f - g_pred[2]) / 2.0f);
         delta_q_acc.j = 0.0f;
-        delta_q_acc.k = G_pred[0] / sq_gk;
+        delta_q_acc.k = -g_pred[0] / sq_gk;
     }
 
 
@@ -116,21 +112,20 @@ void DSP_AHRS_NC_FilterUpdate_f32(const DSP_AHRS_NC_Instance_f32* filter,
     }
 
     // Applying accelerometer correction
-    DSP_QT_Multiply_f32(&data->AttitudeEstimate, &q_gyro, &delta_q_acc);
+    DSP_QT_Multiply_f32(&data->AttitudeEstimate, &delta_q_acc, &q_gyro);
     DSP_QT_Normalize_f32(&data->AttitudeEstimate, &data->AttitudeEstimate);
 
     /*
     ** Magnetometer yaw correction
-    ** By default magnetometer readings are set to (1, 0, 0)
-    ** to remove any yaw changes if actual sensor measurements are unavailable.
     */
 
-    DSP_Quaternion_f32 q_acc_LG;
-    DSP_QT_Conjugate_f32(&q_acc_LG, &data->AttitudeEstimate);
+    if(filter->MagGain == 0.0f)
+    {
+        return;
+    }
 
     float l[3] = {data->MagData[0], data->MagData[1], data->MagData[2]};
-
-    DSP_QT_RotateVector_f32(l, l, &q_acc_LG);
+    DSP_QT_RotateVector_f32(l, l, &data->AttitudeEstimate);
 
     DSP_Quaternion_f32 delta_q_mag;
     const float gamma    = sqrtf(l[0] * l[0] + l[1] * l[1]);
@@ -141,14 +136,14 @@ void DSP_AHRS_NC_FilterUpdate_f32(const DSP_AHRS_NC_Instance_f32* filter,
         delta_q_mag.r = sqrtf(gamma + l[0] * sq_gamma) / (sqrtf(2.0f) * sq_gamma);
         delta_q_mag.i = 0.0f;
         delta_q_mag.j = 0.0f;
-        delta_q_mag.k = l[1] / sqrtf(2.0f * (gamma + l[0] * sq_gamma));
+        delta_q_mag.k = -l[1] / sqrtf(2.0f * (gamma + l[0] * sq_gamma));
     }
     else
     {
         delta_q_mag.r = l[1] / sqrtf(2.0f * (gamma - l[0] * sq_gamma));
         delta_q_mag.i = 0.0f;
         delta_q_mag.j = 0.0f;
-        delta_q_mag.k = sqrtf(gamma - l[0] * sq_gamma) / (sqrtf(2.0f) * sq_gamma);
+        delta_q_mag.k = -sqrtf(gamma - l[0] * sq_gamma) / (sqrtf(2.0f) * sq_gamma);
     }
 
     if(delta_q_mag.r > filter->SLERP_Threshold)
@@ -169,6 +164,6 @@ void DSP_AHRS_NC_FilterUpdate_f32(const DSP_AHRS_NC_Instance_f32* filter,
 
 
     // Applying magnetometer correction
-    DSP_QT_Multiply_f32(&data->AttitudeEstimate, &data->AttitudeEstimate, &delta_q_mag);
+    DSP_QT_Multiply_f32(&data->AttitudeEstimate, &delta_q_mag, &data->AttitudeEstimate);
     DSP_QT_Normalize_f32(&data->AttitudeEstimate, &data->AttitudeEstimate);
 }
